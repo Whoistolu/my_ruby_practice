b35ff3c1f1d9face4afd02fe14d5e69bb2ee9a9f
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-05 23:54:59 +0100

    add new files

--- index.rb
+++ index.rb
@@ -4,11 +4,16 @@ def caesar_cipher(str, shift=1)
   #we will loop through each of the strings in order to accomplish the new string and then push it to caesar
 
   str.each_char do |letter|
+
     if letter = " "
         caesar += " "
     else
-        old_idx = alphabeth.find_index(letter)
-        new_idx = ( old_idx + shift ) % alphabeth.count
-        
+        old_idx = alphabet.find_index(letter)
+        new_idx = ( old_idx + shift ) % alphabet.count
+        caesar += alphabet[new_idx]
     end
+  end
+    
+
+  caesar
 end
