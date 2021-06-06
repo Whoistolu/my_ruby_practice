9e9d8cefcb786746b17b310de8c7e81d00aceac8
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-06 22:58:39 +0100

    add new files

--- index.rb
+++ index.rb
@@ -17,3 +17,21 @@ def caesar_cipher(str, shift=1)
 
   caesar_cipher("abc abc", 4)
 end
+
+ALPHABET_SIZE = 26
+
+def caesar_cipher(string)
+  shiftyArray = []
+  charLine = string.chars.map(&:ord)
+
+  shift = 1
+  ALPHABET_SIZE.times do |shift|
+    shiftyArray << charLine.map do |c|
+      ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
+    end.join
+  end
+
+  shiftyArray
+end
+
+puts caesar_cipher("testing")
