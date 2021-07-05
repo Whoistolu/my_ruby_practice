3bb66696a803e0f4f35f1a4eeafa5b9fb7f77aa2
Toluwashe Ajise <digittolu25@gmail.com>
2021-07-06 00:40:34 +0100

    add new files

--- index.rb
+++ index.rb
@@ -7,8 +7,8 @@ puts ("Hello " + name + ", We are glad to have you on board")
 # Building a simple calculator
 
 puts "Enter a number"
-first_num = gets.chomp()
+first_num = gets.chomp().to_f
 puts "Kindly enter another number"
-second_number = gets.chomp()
+second_number = gets.chomp().to_f
 
-puts (first_num.to_f + second_number.to_f)
+puts (first_num + second_number)
