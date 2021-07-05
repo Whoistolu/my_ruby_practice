870b0be5c2068d898c623e9162f10d178b57aac8
Toluwashe Ajise <digittolu25@gmail.com>
2021-07-06 00:39:18 +0100

    add new files

--- index.rb
+++ index.rb
@@ -2,5 +2,14 @@
 
 puts "Can we have your name? "
 name = gets.chomp()
-puts ("Hello " + name + ", We are glad to have you on board") 
+puts ("Hello " + name + ", We are glad to have you on board")
+
+# Building a simple calculator
+
+puts "Enter a number"
+first_num = gets.chomp()
+puts "Kindly enter another number"
+second_number = gets.chomp()
+
+puts (first_num + second_number)
 
