4c9e2a8ef82ceb5cedc5b4e171168537021b0c43
Toluwashe Ajise <digittolu25@gmail.com>
2021-07-06 14:48:43 +0100

    modify method

--- method/say-hi.rb
+++ method/say-hi.rb
@@ -1,5 +1,5 @@
 def sayhi(name, age)
-    puts ("How are you doing today mr. " + name + ", you are" + age + "years old sir") 
+    puts ("How are you doing today mr. " + name + ", you are " + age.to_s + " years old sir") 
 end
 
 sayhi("Toluwase", 26)
