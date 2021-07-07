a30131058e65fb862b5cb90eb305fbbe7316a07a
Toluwashe Ajise <digittolu25@gmail.com>
2021-07-07 10:17:53 +0100

    add a better calculator

---
+++ better_calculator/better-calc.rb
@@ -0,0 +1,19 @@
+puts ("Enter a number")
+num1 = gets.chomp().to_f
+puts ("Enter a operator")
+op = gets.chomp()
+puts ("Enter a number")
+num2 = gets.chomp().to_f
+
+if op == "+"
+    puts (num1 + num2)
+elsif op == "-"
+    puts (num1 - num2)
+elsif op == "*"
+    puts (num1 * num2)
+elsif op == "/"
+    puts (num1 / num2)
+else
+    puts ("Invalid operator")
+    
+end
