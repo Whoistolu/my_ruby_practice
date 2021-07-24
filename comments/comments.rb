5645af3309242367fe22a67e0df8f80aa334e7ff
Whoistolu <digittolu25@gmail.com>
2021-07-24 10:12:11 +0100

    add modification

---
+++ comments/comments.rb
@@ -0,0 +1,6 @@
+# This is the typical way to make simple comments in ruby
+
+=begin
+Comment block in ruby
+
+=end
--- exponent_method/exp_method.rb
+++ exponent_method/exp_method.rb
@@ -1,6 +1,6 @@
 def pow(base_num, pow_num)
     result = 1
-    pow_num.times do |index|
+    pow_num.times do
         result = result * base_num
     end
 
