69acfcfe3e4d6892cf8f818868b1f2f00f61acca
Whoistolu <digittolu25@gmail.com>
2021-07-25 23:27:52 +0100

    add modification

---
+++ handling_errors/error.rb
@@ -0,0 +1,9 @@
+# dividing by zero throws an error
+
+num = 10 / 0
+
+# can not pass a string into the array of numbers, it throws an error also
+
+list_of_numbers = [2, 34, 55, 898]
+
+list_of_numbers["dog"]
