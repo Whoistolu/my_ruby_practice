5c337edd471fd5ed881b50cb4be872427d5aa1a2
Whoistolu <digittolu25@gmail.com>
2021-07-25 23:37:47 +0100

    add handling errors

--- handling_errors/error.rb
+++ handling_errors/error.rb
@@ -1,9 +1,17 @@
 # dividing by zero throws an error
 
-num = 10 / 0
+#num = 10 / 0
 
 # can not pass a string into the array of numbers, it throws an error also
 
 list_of_numbers = [2, 34, 55, 898]
 
 list_of_numbers["dog"]
+
+begin
+    list_of_numbers["dog"]
+    # num = 10 / 0
+rescue ZeroDivisionError
+    puts "A division by zero error"
+end
+
