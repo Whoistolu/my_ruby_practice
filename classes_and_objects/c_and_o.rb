1de2cb55f53064923c81a224e7d0fa1160d3bf54
Whoistolu <digittolu25@gmail.com>
2021-07-26 00:55:18 +0100

    add class and objects

--- classes_and_objects/c_and_o.rb
+++ classes_and_objects/c_and_o.rb
@@ -1,3 +1,4 @@
+
 class Vehicle
     attr_accessor :engine, :doors, :stiring
 end
@@ -6,3 +7,10 @@ vehicle1 = Vehicle.new()
 vehicle1.engine = "German engine"
 vehicle1.doors = "Chineese doors"
 vehicle1.stiring = "made in Italy"
+
+vehicle2 = Vehicle.new()
+vehicle2,engine = "Thaiwan made"
+vehicle2.doors = "made in Pakistan"
+vehicle2.stiring = "Tesla product"
+
+puts vehicle2.engine
