090442b98858125af6e0b98fd5c19b398dc57d22
Whoistolu <digittolu25@gmail.com>
2021-07-26 23:35:08 +0100

    add initialize method

---
+++ initialize_method/initialize.rb
@@ -0,0 +1,19 @@
+
+class Vehicle
+    attr_accessor :engine, :doors, :stiring
+    def initialize()
+        puts "Creating vehicle"
+    end
+end
+
+vehicle1 = Vehicle.new()
+vehicle1.engine = "German engine"
+vehicle1.doors = "Chineese doors"
+vehicle1.stiring = "made in Italy"
+
+vehicle2 = Vehicle.new()
+vehicle2,engine = "Thaiwan made"
+vehicle2.doors = "made in Pakistan"
+vehicle2.stiring = "Tesla product"
+
+puts vehicle2.engine
