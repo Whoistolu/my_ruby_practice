a10c812483596020d1fe186a62fa8f12035d1ffa
Whoistolu <digittolu25@gmail.com>
2021-07-28 01:58:19 +0100

    add new files

--- each/each.rb
+++ each/each.rb
@@ -1,3 +1,10 @@
-friends = ["haastrup", "bolu", "femi", "deola", "ahmed"]
+# friends = ["haastrup", "bolu", "femi", "deola", "ahmed"]
 
-friends.each {|friend| puts "Meet my good pal, " + friend}
+# friends.each {|friend| puts "Meet my good pal, " + friend}
+
+my_array = [15, 2]
+
+my_array.each do |num|
+  num *= 2
+  puts "The new number is #{num}."
+end
